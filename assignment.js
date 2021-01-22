5b1c61db78357664173945fb215eb9af17aafee5
kawcher <kawcher578@gmail.com>
2021-01-22 17:09:25 +0600

    finish

--- assignment.js
+++ assignment.js
@@ -2,31 +2,56 @@
 
 function kilometerToMeter(inputValue){
             
-            var kilometer = inputValue;
-            var meter = kilometer * 1000;
-            return meter;
-
+     if(inputValue <= 0){
+          return "invalid number";
+     }
+     else{
+          var kilometer = inputValue;
+          var meter = kilometer * 1000;
+          return meter;
+     }
 }
-    var result = kilometerToMeter(10);
+    var result = kilometerToMeter(30);
+     
 
 //Problem No -2 Budget Calculator
 
 function budgetCalculator(watch, phone, laptop){
-        var watchPrice = watch * 50;
-        var phonePrice = phone * 100;
-        var laptopPrice = laptop * 500;
-        return watchPrice + phonePrice + laptopPrice;
+
+     if(watch <= 0 && phone <= 0 && laptop <= 0){
+               return 'Your Qty Number Is Invalid';
+
+     }else if(watch <= 0){
+               return 'Your Watch Qty Number Is Invalid';
+     }
+     else if(phone <= 0){
+               return 'Your Phone Qty Number Is Invalid';
+     }
+     else if(laptop <= 0){
+               return 'Your Laptop Qty Number Is Invalid';
+     }
+     else{
+          var watchPrice = watch * 50;
+          var phonePrice = phone * 100;
+          var laptopPrice = laptop * 500;
+          return watchPrice + phonePrice + laptopPrice;
+     }
+
 }
-    var totalBudget = budgetCalculator(10,7,4);
-    console.log(totalBudget)
+    var totalBudget = budgetCalculator(12,7,6);
+    
+    
 
 // Problem No -3 Hotel Cost
 
 function hotelCost(days){
-        var cost = 0;
-        if(days<=10){
+          if(days <= 0){
+               return 'Invalid Days';
+          }
+     var cost = 0;
+        if(days <= 10){
             cost = days * 100;
-        }else if(days<=20){
+        }else if(days <= 20){
             var firstTenDay = 10 * 100;
             var remainingDays = days - 10;
             var secondTenDay = remainingDays * 80;
@@ -38,9 +63,11 @@ function hotelCost(days){
             var nextDays = remainingDays * 50;
             cost = firstTenDay + secondTenDay + nextDays;
         }
-       return cost;
+     return cost;
 }
-    var totalCost = hotelCost(90);
+    var totalCost = hotelCost(-5);
+    
+    
     
 //Problem No -4 megaFriend
 
@@ -51,12 +78,12 @@ function megaFriend(arrayValue) {
         var element = arrayValue[i].length;
         if(element > maxLength){
             maxLength = element;
-            largeName = arrayValue[i]
-        }
- 
+            largeName = arrayValue[i];
+          }
      }
      return maxLength, largeName;
  }
-     var frindsName =["Kawcher","Habib","Adnan-Habib","Rafi","Adnan-Rafi"];
+
+     var frindsName =["Kawcher","Habib","Adnan Habib","Rafi","Adnan Rafi","Kayser Hamid"];
      var largeLengthName = megaFriend(frindsName);
-     
+ 
